a79230da601a79ba1391ecbc8071d6f2143677d3
Bonganibg <bongani.gumbo@yahoo.com>
2022-05-23 09:45:05 +0200

    push to github

--- models/app.js
+++ models/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routeProject = require('./routes/project');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 var localDB = 'mongodb://localhost:27017/bonganibg-testing';
 
